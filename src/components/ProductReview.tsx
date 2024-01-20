5dd583b350b0855e73b935a8a2fc6597a65eacdc
unknown <samiraborsha08@gmail.com>
2024-01-21 04:30:37 +0600

    feat: cache revalidation while posting a comment

--- src/components/ProductReview.tsx
+++ src/components/ProductReview.tsx
@@ -1,16 +1,12 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, Key, useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
 import { FiSend } from 'react-icons/fi';
-import { usePostCommentMutation } from '@/redux/api/apiSlice';
-
-const dummyComments = [
-  'Bhalo na',
-  'Ki shob ghori egula??',
-  'Eta kono product holo ??',
-  '200 taka dibo, hobe ??',
-];
+import {
+  useGetCommentQuery,
+  usePostCommentMutation,
+} from '@/redux/api/apiSlice';
 
 interface IProps {
   id: string;
@@ -21,7 +17,7 @@ export default function ProductReview({ id }: IProps) {
 
   const [postComment, { isLoading, isError, isSuccess }] =
     usePostCommentMutation();
-
+  const {data} = useGetCommentQuery(id);
   console.log(isLoading, isSuccess, isError);
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -55,7 +51,7 @@ export default function ProductReview({ id }: IProps) {
         </Button>
       </form>
       <div className="mt-10">
-        {dummyComments.map((comment, index) => (
+        {data?.comments?.map((comment: string, index: number ) => (
           <div key={index} className="flex gap-3 items-center mb-5">
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" />
